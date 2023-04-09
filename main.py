0bc25d99f13ab88417e9e58b313e4938a8478b0f
Damien Gonot <damien.gonot@gmail.com>
2023-04-08 22:04:06 -0400

    add missing import

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from email.message import EmailMessage
 import google.auth
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
